65dd2c6fbe242f15e2011a1c91a9dc679c836eef
Furysan-IA <maximiliano@riosglobalexperts.com>
2025-08-03 23:07:04 -0300

    Create separate public QR application

--- src/hooks/useSupabaseConnection.ts
+++
@@ -1,4 +0,0 @@
-// This file is now empty as all Excel parsing functionality has been moved to excelParsingService.ts
-// Keep this file for potential future Supabase connection hooks
-
-export {};
--- src/utils/excelParser.ts
+++
@@ -1,2 +0,0 @@
-// Re-export everything from the new service module
-export * from './excelParsingService';
